b985dbc158948a63692d0c521a649b8b958fdb4e
Massimiliano Vignoli <massimiliano.vignoli@bonfiglioli.com>
2020-07-18 14:45:55 +0200

    test polling

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import subprocess
 
 app = Flask(__name__)
 CORS(app)
+i = 1
 
 @app.route('/barcode', methods = ['GET'])
 def barcodeReader():
@@ -16,10 +17,19 @@ def barcodeReader():
 @app.route('/automation', methods = ['GET'])
 def executeAutomation():
   if request.method == 'GET':
-    import os
-    subprocess.Popen('explorer')
-    #res = execute()
     return json.dumps({'res': 1}), 200, {'ContentType':'application/json'}
+
+@app.route('/settemp', methods = ['GET'])
+def settemp():
+  if request.method == 'GET':
+    return json.dumps({'res': 1}), 200, {'ContentType':'application/json'}
+
+@app.route('/check', methods = ['GET'])
+def check():
+  if request.method == 'GET':
+    global i
+    i +=1
+    return json.dumps({'status': i%5==0, 'res':5}), 200, {'ContentType':'application/json'}
 
 if __name__ == '__main__':
   app.debug = True

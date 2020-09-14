ad13f3de289013814d4a5f351d0ebd0961fb83ab
federico <federico.insero@mail.polimi.it>
2020-09-14 12:32:17 +0200

    checked and fixed protocol C

--- Protocols_definitive/Station C/protocol_C.py
+++ Protocols_definitive/Station C/protocol_C.py
@@ -7,54 +7,69 @@ from typing import Optional
 from opentrons.protocol_api import ProtocolContext
 from threading import Thread
 import time
+import datetime
 
-
-class BlinkingLight(Thread):
-    def __init__(self, ctx: ProtocolContext, t: float = 1):
-        super(BlinkingLight, self).__init__()
-        self._on = False
-        self._state = True
-        self._ctx = ctx
-        self._t = t
-    
-    def stop(self):
-        self._on = False
-        self.join()
-
-    def switch(self, x: Optional[bool] = None):
-        self._state = not self._state if x is None else x
-        self._ctx._hw_manager.hardware.set_lights(rails=self._state)
-            
-    def run(self):
-        self._on = True
-        state = self._ctx._hw_manager.hardware.get_lights()
-        while self._on:
-            self.switch()
-            time.sleep(self._t)
-        self.switch(state)
+# For setting and checking the temperature
+temp_a = 3.9
+temp_check = 4.0
+TempUB = temp_check + 0.3
 
 
 # metadata
 metadata = {
     'protocolName': 'Version 1 S9 Station C Technogenetics P20 Multi',
-    'author': 'Nick Pootza<protocols@opentrons.com>',
+    'author': 'Marco & Giada',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.3'
 }
 
-
-NUM_SAMPLES = 16  # start with 8 samples, slowly increase to 48, then 94 (max is 94)
-TIP_TRACK = False
+NUM_SAMPLES = 96  # start with 8 samples, slowly increase to 48, then 94 (max is 94)
+NUM_SEDUTE = 1
+TIP_TRACK = True
 MM_PER_SAMPLE = 20
 SAMPLE_VOL = 20
 
+liquid_headroom = 1.1
+mm_tube_capacity = 1450
+mm_strips_capacity = 180
+
 
 def run(ctx: protocol_api.ProtocolContext):
+    # Define the Path for the log temperature file
+    folder_path = '/var/lib/jupyter/notebooks/outputs'
+    temp_file_path = folder_path + '/completion_log.json'
+    # FIXME: check this line: without this we can't append the data of the log
+    Log_Dict = {"stages": []}  # For log file data
+    current_status = "Setting Temperature"
+
+    def update_log_file(status="SUCCESS", check_temperature=True, message=None):
+        current_Log_dict = {"stage_name": current_status,
+                            "time": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f"),
+                            "temp": None,
+                            "status": status,
+                            "message": None}
+        if check_temperature:
+            current_Log_dict["temp"] = tempdeck.temperature
+            if tempdeck.temperature >= TempUB and tempdeck.status != 'holding at target':
+                # if tempdeck.status != 'holding at target':
+                while tempdeck.temperature >= temp_check:
+                    print("sleeping for 0.5 s to wait for Temp_Deck")
+                    print("current temperature is {}°C".format(tempdeck.temperature))
+                    time.sleep(0.5)
+
+                current_Log_dict["status"] = "FAILED"
+                current_Log_dict["message"] = "Temperature rose above threshold value"
+
+        Log_Dict["stages"].append(current_Log_dict)
+        if not os.path.isdir(folder_path):
+            os.mkdir(folder_path)
+        with open(temp_file_path, 'w') as outfiletemp:
+            json.dump(Log_Dict, outfiletemp)
+
+        print('{}: {}'.format(current_status, message))
     global MM_TYPE
 
     ctx.comment("Station C Technogenetics protocol for {} samples.".format(NUM_SAMPLES))
-    mm_tot_vol = NUM_SAMPLES * MM_PER_SAMPLE * 1.1
-    ctx.comment("Mastermix expected volume: {} ul.".format(mm_tot_vol))
 
     # check source (elution) labware type
     source_plate = ctx.load_labware(
@@ -65,8 +80,7 @@ def run(ctx: protocol_api.ProtocolContext):
         for slot in ['2', '3', '6', '7', '9']
     ]
     tips20_no_a = [
-        ctx.load_labware('opentrons_96_filtertiprack_20ul', '11',
-        '20µl tiprack - no tips in row A')
+        ctx.load_labware('opentrons_96_filtertiprack_20ul', '11', '20µl tiprack - no tips in row A')
     ]
     tips300 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', '10')]
     tempdeck = ctx.load_module('Temperature Module Gen2', '4')
@@ -75,22 +89,21 @@ def run(ctx: protocol_api.ProtocolContext):
     mm_strips = ctx.load_labware(
         'opentrons_96_aluminumblock_generic_pcr_strip_200ul', '8',
         'mastermix strips')
-    tempdeck.set_temperature(4)
+    tempdeck.set_temperature(temp_a)
     tube_block = ctx.load_labware(
         'opentrons_24_aluminumblock_nest_1.5ml_snapcap', '5',
         '2ml screw tube aluminum block for mastermix + controls')
-    
+
     # pipette
     m20 = ctx.load_instrument('p20_multi_gen2', 'right', tip_racks=tips20)
     p300 = ctx.load_instrument('p300_single_gen2', 'left', tip_racks=tips300)
 
     # setup up sample sources and destinations
-    num_cols = math.ceil(NUM_SAMPLES/8)
+    num_cols = math.ceil(NUM_SAMPLES / 8)
     sources = source_plate.rows()[0][:num_cols]
     sample_dests = pcr_plate.rows()[0][:num_cols]
 
     tip_log = {'count': {}}
-    folder_path = '/data/C'
     tip_file_path = folder_path + '/tip_log.json'
     if TIP_TRACK and not ctx.is_simulating():
         if os.path.isfile(tip_file_path):
@@ -145,34 +158,49 @@ before resuming.')
         # print("Picked up {} with {} [#{}]".format(tip_log['tips']['tips20_no_a'][tip_log['count']['tips20_no_a']-1], 'tips20_no_a', tip_log['count']['tips20_no_a']))
 
     """ mastermix component maps """
-    mm_tube = tube_block.wells()[0]
-    
-    mm_strip = mm_strips.columns()[0]
-    remaining_samples = NUM_SAMPLES
+    # setup tube mastermix
+    num_mm_tubes = math.ceil(MM_PER_SAMPLE * NUM_SAMPLES * liquid_headroom / mm_tube_capacity)
+    mm_cols_per_tube = math.ceil(NUM_SAMPLES / num_mm_tubes)
+    mm_tube = tube_block.columns()[0][:num_mm_tubes]
+    ctx.comment("Mastermix: using {} tubes with at least {:.2f} mL each".format(
+        num_mm_tubes, mm_cols_per_tube * MM_PER_SAMPLE * liquid_headroom / 1000))
+
+    # setup strips mastermix
+    num_mm_strips = math.ceil(MM_PER_SAMPLE * num_cols * liquid_headroom / mm_strips_capacity)
+    mm_cols_per_strips = math.ceil(num_cols / num_mm_strips)
+    mm_strip = mm_strips.columns()[:num_mm_strips][0]
+    # ctx.comment('Vediamo se stampa quello che vogliamo {}'.format(mm_strip))
+    remaining_samples = NUM_SAMPLES * NUM_SEDUTE
 
     # for _ in range(5):
     #    test_light = BlinkingLight(ctx)
     #    test_light.start()
     #    ctx.delay(30)
     #    test_light.stop()
-    
-    #### START REPEATED SECTION
+
+    # START REPEATED SECTION
     while remaining_samples > 0:
-        vol_per_strip_well = min(remaining_samples, 96)/8*MM_PER_SAMPLE*1.1
-        
+        vol_per_strip_well = min(remaining_samples, 96) / 8 * MM_PER_SAMPLE * 1.1
+
         # transfer mastermix to strips
+        current_status = 'transfer mastermix to strips'
         pick_up(p300)
         for well in mm_strip:
             p300.transfer(vol_per_strip_well, mm_tube, well, new_tip='never')
         p300.drop_tip()
-    
+
+        update_log_file()
         # transfer mastermix to plate
+        current_status = 'transfer mastermix to plate'
         pick_up(m20)
         m20.transfer(MM_PER_SAMPLE, mm_strip[0].bottom(0.5), sample_dests,
                      new_tip='never')
         m20.drop_tip()
-    
+
+        update_log_file()
+
         # transfer samples to corresponding locations
+        current_status = 'transfer samples to corresponding locations'
         for i, (s, d) in enumerate(zip(sources, sample_dests)):
             # print(i, end=" ")
             if i == 9:
@@ -185,16 +213,16 @@ before resuming.')
             m20.aspirate(5, d.top(2))  # suck in any remaining droplets on way to trash
             m20.drop_tip()
             remaining_samples -= 8
-        
+
+        update_log_file()
+
         if remaining_samples > 0:
-            blight = BlinkingLight(ctx=ctx)
-            blight.start()
             ctx.home()
-            # print("Please, load a new plate from station B. Resume when it is ready")
-            ctx.pause("Please, load a new plate from station B. Resume when it is ready")
-            blight.stop()
-    #### END REPEATED SECTION
-    
+            current_status = "remaining samples >0"
+            update_log_file()
+    # END REPEATED SECTION
+
+    current_status = "finished protocol C"
     # track final used tip
     if TIP_TRACK and not ctx.is_simulating():
         if not os.path.isdir(folder_path):
@@ -207,7 +235,11 @@ before resuming.')
         with open(tip_file_path, 'w') as outfile:
             json.dump(data, outfile)
 
+    ctx.home()
+    update_log_file()
+
 
 if __name__ == "__main__":
-    from opentrons import simulate  
+    from opentrons import simulate
+
     run(simulate.get_protocol_api(metadata["apiLevel"]))

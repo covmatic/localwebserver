faaf11b2e2a56d8b81a4e06686bf96b0f2e7082d
federico <federico.insero@mail.polimi.it>
2020-09-28 11:43:28 +0200

    Added the Licence to the main files

--- services/task_runner.py
+++ services/task_runner.py
@@ -65,6 +65,8 @@ def start_scheduler(app_ctx):
 @scheduler.job(interval=timedelta(seconds=5))
 def check_new_tasks():
     # print("Checking new tasks")
+    # FIXME: CHECK IF THE GLOBAL local_filepath works!!
+    global local_filepath
     protocol = Protocol.query.filter_by(status="queued").first()
     if protocol is not None:
         protocol.set_running()
@@ -259,3 +261,23 @@ def check_new_tasks():
             session.add(protocol)
             session.commit()
             print(e)
+
+
+""" Copyright (c) 2020 Covmatic.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+   sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+ or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+"""

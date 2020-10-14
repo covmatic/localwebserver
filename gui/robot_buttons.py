77d1e09c592a596c909a6d6ef3a2721c90dcebff
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2020-10-14 13:30:51 +0200

    change button order

--- gui/robot_buttons.py
+++ gui/robot_buttons.py
@@ -11,51 +11,6 @@ class RobotButtonFrame(ButtonFrameBase):
     pass
 
 
-class LightsButton(ColorChangingButton, metaclass=RobotButtonFrame.button):
-    text = "Robot Lights"
-    endpoint = ":31950/robot/lights"
-    
-    @property
-    def url(self) -> str:
-        return "http://{}{}".format(_ot_2_ip, self.endpoint)
-    
-    @property
-    def state(self) -> bool:
-        try:
-            response = requests.get(self.url)
-        except requests.exceptions.ConnectionError:
-            state = False
-        else:
-            try:
-                state = response.json().get("on", False)
-            except json.decoder.JSONDecodeError:
-                state = False
-        return state
-    
-    @state.setter
-    def state(self, value: bool):
-        try:
-            requests.post(self.url, json={'on': value})
-        except requests.exceptions.ConnectionError:
-            pass
-        self.update()
-
-
-class HomeButton(metaclass=RobotButtonFrame.button):
-    text = "Home"
-    endpoint = ":31950/robot/home"
-    
-    @property
-    def url(self) -> str:
-        return "http://{}{}".format(_ot_2_ip, self.endpoint)
-    
-    def command(self):
-        try:
-            requests.post(self.url, json={'target': 'robot'})
-        except requests.exceptions.ConnectionError:
-            pass
-
-
 class UpdateSystem(ColorChangingButton, metaclass=RobotButtonFrame.button):
     remote_dir: str = "/var/lib/jupyter/notebooks"
     system_version_file: str = "system_version.py"
@@ -113,3 +68,48 @@ class UpdateSystem(ColorChangingButton, metaclass=RobotButtonFrame.button):
                 _, sdtout, _ = client.exec_command("python -m pip install{} covid19-system9 --upgrade".format(self.pypi_index()))
                 sdtout.read()
         self.update()
+
+
+class LightsButton(ColorChangingButton, metaclass=RobotButtonFrame.button):
+    text = "Robot Lights"
+    endpoint = ":31950/robot/lights"
+    
+    @property
+    def url(self) -> str:
+        return "http://{}{}".format(_ot_2_ip, self.endpoint)
+    
+    @property
+    def state(self) -> bool:
+        try:
+            response = requests.get(self.url)
+        except requests.exceptions.ConnectionError:
+            state = False
+        else:
+            try:
+                state = response.json().get("on", False)
+            except json.decoder.JSONDecodeError:
+                state = False
+        return state
+    
+    @state.setter
+    def state(self, value: bool):
+        try:
+            requests.post(self.url, json={'on': value})
+        except requests.exceptions.ConnectionError:
+            pass
+        self.update()
+
+
+class HomeButton(metaclass=RobotButtonFrame.button):
+    text = "Home"
+    endpoint = ":31950/robot/home"
+    
+    @property
+    def url(self) -> str:
+        return "http://{}{}".format(_ot_2_ip, self.endpoint)
+    
+    def command(self):
+        try:
+            requests.post(self.url, json={'target': 'robot'})
+        except requests.exceptions.ConnectionError:
+            pass
